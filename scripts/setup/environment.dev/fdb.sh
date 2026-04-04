bf38812daab00cd10dcd795082388eb995516fbf
tuyennguyentech <tuyennguyen.tech@gmail.com>
2026-04-04 09:29:20 +0000

    feat: set up FoundationDB dev cluster via compose

---
+++ scripts/setup/environment.dev/fdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu;
+
+FDB_CLUSTER_FILE="fdb.cluster"
+
+# Attempt to connect. Configure the database if necessary.
+if ! /usr/bin/fdbcli -C $FDB_CLUSTER_FILE --exec status --timeout 10 ; then
+    echo "creating the database"
+    if ! fdbcli -C $FDB_CLUSTER_FILE --exec "configure new double ssd ; status" --timeout 10 ; then
+        echo "Unable to configure new FDB cluster."
+        exit 1
+    fi
+fi
